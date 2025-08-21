483d5be2f2bce9a097ad78f34054d96c1fce2540
Fabián Montoya <fabianmontoya2802@gmail.com>
2025-08-20 20:31:51 -0600

    Refactor RPC request handling to include headers and update client method from ping to pong

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { client } from '@/lib/orpc';
 
 export default async function Home() {
   const salut = await client.hello({ name: 'fabian' });
-  const ping = await client.ping();
+  const ping = await client.pong();
   return (
     <div>
       {salut} {ping}
