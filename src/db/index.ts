d8d02b743c1075e76dbddb98f68a0c333d5709af
Fabián Montoya <fabianmontoya2802@gmail.com>
2025-08-20 20:11:38 -0600

    Add ORPC integration with new RPC routes and client setup; update biome configuration and dependencies

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { DATABASE_URL } from '../../env-config';
-// biome-ignore lint/performance/noNamespaceImport: It's required
 import * as schema from './schema';
 
 const sql = neon(DATABASE_URL);
