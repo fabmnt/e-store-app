ad15e93089e47ee18da0d79730da6cd323d94019
Fabián Montoya <fabianmontoya2802@gmail.com>
2025-08-30 11:58:56 -0600

    style: enhance categories navigation layout and button variants
    - Updated the categories navigation component to allow for horizontal scrolling and improved spacing.
    - Adjusted button variants to reflect active states based on the current pathname, enhancing user feedback and visual clarity.

--- src/features/categories/components/categories-navigation.tsx
+++ src/features/categories/components/categories-navigation.tsx
@@ -20,30 +20,33 @@ export function CategoriesNavigation() {
   const categories = store.categories;
 
   return (
-    <nav className="flex items-center xl:gap-x-6">
+    <nav className="flex max-w-[360px] items-center gap-x-4 overflow-x-auto xl:gap-x-6">
       <Button
         asChild
         className={cn(
-          'relative rounded-none px-5 py-5 tracking-tight transition-all hover:font-semibold xl:px-10',
-          pathname === `/s/${storeSlug}` && 'font-semibold'
+          'relative px-5 py-5 tracking-tight transition-all hover:font-semibold xl:px-10'
         )}
-        variant="ghost"
+        variant={pathname === `/s/${storeSlug}` ? 'default' : 'secondary'}
       >
         <Link href={`/s/${storeSlug}/`}>
           <span>Todos</span>
-          {pathname === `/s/${storeSlug}` && (
+          {/*           {pathname === `/s/${storeSlug}` && (
             <span className="absolute bottom-0 left-0 h-[2px] w-full bg-primary" />
-          )}
+          )} */}
         </Link>
       </Button>
       {categories.map((category) => (
         <Button
           asChild
           className={cn(
-            'relative rounded-none px-5 py-5 tracking-tight transition-all hover:font-semibold xl:px-10'
+            'relative px-5 py-5 tracking-tight transition-all hover:font-semibold xl:px-10'
           )}
           key={category.id}
-          variant="ghost"
+          variant={
+            pathname === `/s/${storeSlug}/${category.slug}`
+              ? 'default'
+              : 'secondary'
+          }
         >
           <Link
             className={cn(
@@ -52,9 +55,9 @@ export function CategoriesNavigation() {
             href={`/s/${storeSlug}/${category.slug}`}
           >
             <span>{category.name}</span>
-            {pathname === `/s/${storeSlug}/${category.slug}` && (
+            {/*             {pathname === `/s/${storeSlug}/${category.slug}` && (
               <span className="absolute bottom-0 left-0 h-[2px] w-full bg-primary" />
-            )}
+            )} */}
           </Link>
         </Button>
       ))}
